8fde76588e43c8867a62dc007d5841f6b094b505
odegaard.jackson <jackodegaard05@gmail.com>
2021-10-06 10:43:32 -0700

    Initial commit

---
+++ src/org/vashonsd/Main.java
@@ -0,0 +1,12 @@
+package org.vashonsd;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(squareof(16));
+
+    } public static int squareof(int b) {
+
+        return b*b;
+    }
+}
